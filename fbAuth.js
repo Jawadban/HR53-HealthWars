de45161bbba1520bf317668fe30c5d413573e343
Abiy Melaku <agirma08@gmail.com>
2017-02-09 11:08:57 -0800

    modularize fbAuth
    import necessary user model and api keys
    build basic authorization function for export
    build small function for serializing user

---
+++ fbAuth.js
@@ -0,0 +1,16 @@
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongoose = require('mongoose');
+
+//load user model
+var User = require('./server/dbmodules/users/userModel.js');
+
+//load api keys
+var Keys = require('./keys.js');
+
+module.exports = function(passport) {
+  // used to serialize the user for the session
+  passport.serializeUser(function(user, done) {
+    console.log('USER FROM SERIALIZE USER, ', user.facebook.id);
+    done(null, user.facebook.id);
+  });
+}
