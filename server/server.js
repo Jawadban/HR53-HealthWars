7a79e5565056b718c8c213cca08e9526d53e3ab3
Abiy Melaku <agirma08@gmail.com>
2017-02-09 11:25:42 -0800

    Add middleware to make sure a user is logged in

--- server/server.js
+++ server/server.js
@@ -55,3 +55,12 @@ app.get('/logout', function (req, res) {
   });
   res.redirect('/');
 });
+
+//============ route middleware to make sure a user is logged in =============/
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
