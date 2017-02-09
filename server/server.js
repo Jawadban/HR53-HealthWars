df661d2b797517bdbbda5b6b9b95b2358e022b68
Abiy Melaku <agirma08@gmail.com>
2017-02-09 11:11:25 -0800

    configure authorization for fb

--- server/server.js
+++ server/server.js
@@ -19,3 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+//=========configure authentication=========/
+var session = require('express-session');
+var passport = require('passport');
+require('../fbAuth.js')(passport);
+app.use(session({
+  secret: 'kenny',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
