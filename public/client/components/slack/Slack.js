ec14d62b10daf15b7e3366172d3bcbf355a794c6
Max <maxq210@gmail.com>
2017-02-08 20:11:28 -0800

    added more to slack

--- public/client/components/slack/Slack.js
+++ public/client/components/slack/Slack.js
@@ -7,23 +7,37 @@ export default class Slack extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: ''
+      message: '',
+      username: 'incoming_webhook',
+      channel: '#random'
      // users: null, // this is more or less used as a check for new props
     //  data: [] // this is the data to be used to sort the Overview table
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleChannelChange = this.handleChannelChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleMessageChange(event) {
     this.setState({message: event.target.value});
     console.log(this.state.message);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({username: event.target.value});
+    console.log(this.state.username);
+  }
+
+  handleChannelChange(event) {
+    this.setState({channel: event.target.value});
+    console.log(this.state.channel);
   }
 
   handleSubmit(event) {
     var url = "https://hooks.slack.com/services/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";
     $.ajax({
-      data: 'payload=' + JSON.stringify({"text": this.state.message}),
+      data: 'payload=' + JSON.stringify({"username": this.state.username, "text": this.state.message, "channel": this.state.channel}),
       dataType: 'json',
       processData: false,
       type: 'POST',
@@ -36,13 +50,25 @@ export default class Slack extends React.Component {
 
   render() {
     return (
+      <div>
+      <h1>Slack Messages</h1>
+      <p>Message your friends on Slack to update them on your progress or invite them to challenges!</p>
       <form onSubmit={this.handleSubmit}>
         <label>
           Message:
-          <input type="text" value={this.state.message} onChange={this.handleChange} />
+          <input type="text" value={this.state.message} onChange={this.handleMessageChange} />
+        </label>
+        <label>
+          Username:
+          <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+        </label>
+        <label>
+          Channel or @User:
+          <input type="text" value={this.state.channel} onChange={this.handleChannelChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      </div>
     );
   }
 }
