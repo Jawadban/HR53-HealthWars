77159d62ca2d5736cb630d494fb517fc62bbb7ba
Bill Lea <bill.lea@gmail.com>
2017-02-11 11:50:28 -0800

    Moved keys folder

--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'facebook' : {
-    'clientID' : '1858633147742361',
-    'clientSecret' : '05dcf2884a5dd2acb95dfbb8d0bb1cc6',
-    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-  }
-}
