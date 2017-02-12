5bf027c39ebb277aa56c05a8428228034fb3cc2f
Bill Lea <bill.lea@gmail.com>
2017-02-11 17:28:21 -0800

    Exercise page converted to add stars using MySQL

--- server/dbmodules/stars/starControllerSQL.js
+++ server/dbmodules/stars/starControllerSQL.js
@@ -22,4 +22,26 @@ module.exports = {
       });
     },
 
+    newStar : function(req, res, next) {
+      return mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: '',
+          database: 'healthwars'
+      }).then(function(conn){
+          connection = conn;
+          console.log('BODY', req.body);
+          console.log('QUERY', req.params);
+          var sql = `insert into stars values (null, '${req.body.color}', ${req.body.id_users}, ${req.body.id_competition})`;
+          console.log('insert stars sql: ', sql);
+          return connection.query(sql);
+      }).then(function(rows){
+          console.log(rows);
+          res.json(rows);
+          
+      });
+    },
+
+
+
 };
