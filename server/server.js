972db3633f0232afff69a3420dd242a444d0464e
Abiy Melaku <agirma08@gmail.com>
2017-02-12 14:12:56 -0800

    Fix fb redirect error

--- server/server.js
+++ server/server.js
@@ -74,6 +74,6 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) 
         return next();
     // if they aren't redirect them to the home page
-    res.redirect('/auth/login');
+    res.redirect('/');
 }
 
